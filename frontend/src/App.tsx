176530f0b309c032bee9a0cc69bde8c1620f13b5
MWSERVER\k-miyatani <k-miyatani@mediaware.co.jp>
2023-01-05 19:04:27 +0900

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "./component/Layout";
+
+const App = ()=>{
+    return (
+        <Layout>
+         <h2>App</h2>
+        </Layout>
+    )
+}
+export default App;
